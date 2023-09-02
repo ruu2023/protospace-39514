5155ae2a39b2a714b66d8fbfe473bf3f85270a27
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-09-02 17:21:27 +0900

    新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :text, presence: true
-  validates :name, presence: true
-  validates :profile, presence: true
-  validates :occupation, presence: true
-  validates :position, presence: true
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
